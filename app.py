aec1a49d01f52880e98ef0a2b73ba0cc2720e5fd
Shubhamkira10 <167739196+Shubhamkira10@users.noreply.github.com>
2025-03-09 22:18:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,6 @@ async def first_endpoint(response: Response):
     response.headers["Content-Type"] = "application/json"
     response.headers["Authorization"] = "Bearer token123"
     response.status_code = 200
-    return {
-        "headers": {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer token123"
-        },
-        "status": 200
-    }
 
 @app.get("/second")
 async def second_endpoint(response: Response):
@@ -21,10 +14,6 @@ async def second_endpoint(response: Response):
     response.headers["Authorization"] = "Bearer token123"
     response.status_code = 400
     return {
-        "headers": {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer token123"
-        },
         "body": {
             "param1": "value1",
             "param2": "value2"
