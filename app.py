3b56a50a4abcf0ddd919ac2aaff64903aa7f4c93
Shubhamkira10 <167739196+Shubhamkira10@users.noreply.github.com>
2025-03-09 20:40:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Response
+
+app = FastAPI()
+
+@app.get("/first")
+async def first_endpoint(response: Response):
+    response.headers["Content-Type"] = "application/json"
+    response.headers["Authorization"] = "Bearer token123"
+    response.status_code = 200
+    return {"message": "Test1  successful"}
+
+@app.get("/second")
+async def second_endpoint(response: Response):
+    response.headers["Content-Type"] = "application/json"
+    response.headers["Authorization"] = "Bearer token123"
+    response.status_code = 400
+    return {"param1": "value1", "param2": "value2"}
